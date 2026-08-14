bc277745ce55a958bfd0d9e2bbc4d350b4c2f11a
alokranjanpogo <alokranjan18april@gmail.com>
2026-08-14 13:51:29 +0530

    Update 6_Historical_Comparison.py

--- pages/6_Historical_Comparison.py
+++ pages/6_Historical_Comparison.py
@@ -1,1 +1,19 @@
+import streamlit as st
 
+st.title(
+    "📈 Historical Comparison"
+)
+
+st.info(
+    """
+    Compare:
+
+    FY23
+    FY24
+    FY25
+    FY26
+    FY27
+
+    based on rainfall and withdrawal trends.
+    """
+)
