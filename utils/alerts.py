4bb3bca4dc21249abce8d85edff3f67304eb282d
alokranjanpogo <alokranjan18april@gmail.com>
2026-08-14 13:25:40 +0530

    Update alerts.py

--- utils/alerts.py
+++ utils/alerts.py
@@ -1,1 +1,18 @@
+def get_alert(level):
 
+    if level >= 529.5:
+        return {
+            "status": "RED",
+            "message": "MANDATORY WITHDRAWAL 150 MLD"
+        }
+
+    elif level >= 529:
+        return {
+            "status": "YELLOW",
+            "message": "Reservoir Near Alert Level"
+        }
+
+    return {
+        "status": "GREEN",
+        "message": "Normal"
+    }
