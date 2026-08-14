ec284e96fae75f0a11b48fb8075a9bb84fc786ed
alokranjanpogo <alokranjan18april@gmail.com>
2026-08-14 11:33:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,23 @@
+import streamlit as st
 
+st.set_page_config(
+    page_title="Dimna DSS",
+    page_icon="💧",
+    layout="wide"
+)
+
+st.title("💧 Dimna Reservoir Withdrawal Decision Support System")
+
+st.markdown("""
+### Purpose
+
+This dashboard helps:
+
+- Monitor daily Dimna level
+- Track withdrawals
+- Track rainfall
+- Calculate available storage
+- Generate withdrawal alerts
+- Show historical rainfall trends
+- Show historical withdrawal trends
+""")
