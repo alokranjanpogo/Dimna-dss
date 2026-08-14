f6f450397b0d9bccbc0797416981ef47a5048f1a
alokranjanpogo <alokranjan18april@gmail.com>
2026-08-14 13:50:26 +0530

    Update 4_Rainfall_Trend.py

--- pages/4_Rainfall_Trend.py
+++ pages/4_Rainfall_Trend.py
@@ -1,1 +1,22 @@
+import streamlit as st
+import pandas as pd
+import plotly.express as px
 
+st.title(
+    "🌧 Rainfall Trends"
+)
+
+file = "data/rainfall_withdrawal.xlsx"
+
+df = pd.read_excel(
+    file,
+    sheet_name=0
+)
+
+st.dataframe(
+    df.head()
+)
+
+st.info(
+    "Rainfall graphs to be refined after data cleaning."
+)
