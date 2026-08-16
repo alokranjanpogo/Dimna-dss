66a4e0f4e95901ea09114b5bfab998955d50896a
alokranjanpogo <alokranjan18april@gmail.com>
2026-08-17 02:00:54 +0530

    Update 4_Withdrawal_Analytics.py

--- pages/4_Withdrawal_Analytics.py
+++ pages/4_Withdrawal_Analytics.py
@@ -4,17 +4,17 @@ import pandas as pd
 st.title("💦 Withdrawal Analytics")
 
 df = pd.read_excel(
-    "data/rainfall_withdrawal.xlsx",
+    "rainfall_withdrawal.xlsx",
     sheet_name=1,
     header=None
 )
 
 fy = st.selectbox(
     "Select FY",
-    ["FY23", "FY24", "FY25", "FY26", "FY27"]
+    ["FY23","FY24","FY25","FY26","FY27"]
 )
 
-fy_columns = {
+fy_cols = {
     "FY23": 1,
     "FY24": 2,
     "FY25": 3,
@@ -22,22 +22,16 @@ fy_columns = {
     "FY27": 5
 }
 
-chart_df = pd.DataFrame()
-
-chart_df["Date"] = pd.to_datetime(
-    df.iloc[2:, 0],
-    origin="1899-12-30",
-    unit="D",
+withdrawal = pd.to_numeric(
+    df.iloc[2:, fy_cols[fy]],
     errors="coerce"
-)
+).fillna(0)
 
-chart_df["Withdrawal"] = pd.to_numeric(
-    df.iloc[2:, fy_columns[fy]],
-    errors="coerce"
-)
-
-chart_df = chart_df.dropna()
+chart_df = pd.DataFrame({
+    "Day": range(1, len(withdrawal)+1),
+    "Withdrawal": withdrawal.values
+})
 
 st.line_chart(
-    chart_df.set_index("Date")
+    chart_df.set_index("Day")
 )
